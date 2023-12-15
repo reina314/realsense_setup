76eba8e369d5efaab91accfe9cb35dad7de1b3c4
reina <ryugureina314@gmail.com>
2023-12-15 21:44:13 +0900

    add: realsense_setup.sh

---
+++ realsense_setup.sh
@@ -0,0 +1,17 @@
+# Install Packages for Realsense
+sudo mkdir -p /etc/apt/keyrings
+curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
+sudo apt-get install apt-transport-https
+echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/librealsense.list
+sudo apt update
+sudo apt install -y librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg
+
+# Setup Docker Package
+cd ~/catkin_ws/src/
+git clone https://github.com/IntelRealSense/realsense-ros.git
+cd realsense-ros/
+git checkout `git tag | sort -V | grep -P "^2.\d+\.\d+" | tail -1`
+sudo apt-get install ros-noetic-ddynamic-reconfigure
+cd ~/catkin_ws/
+catkin_make
+source devel/setup.bash
